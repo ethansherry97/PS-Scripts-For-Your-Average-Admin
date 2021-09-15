f2057f2a01238396aec8638a55c0acca69fb9e19
ethansherry97 <ethansherry97@gmail.com>
2021-09-15 14:44:31 -0400

    Add files via upload
    This script moves disabled users to a disabled OU / disables them / hides them from the Online GAL. Please see my other script, Account Killer, to use with this

---
+++ MoveAccountsDisableHideFromGAL.ps1
@@ -0,0 +1,24 @@
+﻿## Searches AD and moves expired users to a certain OU
+search-ADAccount -AccountExpired -UsersOnly | move-adobject  -targetpath "ENTER OU HERE"
+
+
+$users = get-adobject -filter {objectclass -eq "user"} -searchbase "ENTER OU HERE" -Properties DistinguishedName,Name,Objectclass,objectguid,description
+
+$date = Get-Date -Format "MM-dd-yyyy"
+
+foreach ($User in $users)
+{
+    $UserID = $user.ObjectGuid
+    
+    Set-ADObject $UserID -replace @{mailNickname=$UserID}
+
+    Set-ADObject $UserID -replace @{msExchHideFromAddressLists=$true}
+    Set-ADObject $UserID -clear ShowinAddressBook
+
+    Disable-ADAccount -Identity $UserID -Confirm:$false
+
+    $UserDescription = get-aduser $UserID -Properties Description |select Description
+    if ($UserDescription.Description -match "Disabled on") {} else {
+    Set-ADUser $UserID -Description "$($UserDescription.Description) (Disabled on $Date)"
+    }
+}
