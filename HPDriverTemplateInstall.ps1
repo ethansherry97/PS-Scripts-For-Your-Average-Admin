02403055d0071d0b6ffba2f2e7c5ca68cad9cfa5
ethansherry97 <ethansherry97@gmail.com>
2023-02-20 13:00:21 -0500

    Add files via upload

---
+++ HPDriverTemplateInstall.ps1
@@ -0,0 +1,19 @@
+﻿$scriptPath = $PSScriptRoot
+$driverPath = Join-path $scriptPath "NAMEOFFILE.EXE"
+
+# Install process
+$process = Start-Process -FilePath $driverPath -ArgumentList "/s" -Wait -PassThru
+$errorcode = $process.ExitCode
+
+
+if ($errorcode -eq 0)
+{
+	Write-Host "Error code: $errorcode"
+	exit 0
+}
+else
+{
+	Write-Host "Installation Failed"
+	Write-Host "Error code: $errorcode"
+	exit $errorcode
+}
